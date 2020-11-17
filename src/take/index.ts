19ecf93801e6c21368196846debd473a9aaab93c
Misha Bruml <misha.bruml@ecotricity.co.uk>
2020-11-17 17:03:29 +0000

    don't need

--- src/take/index.ts
+++
@@ -1,8 +0,0 @@
-export const take = (array: any[], nItems: Number) => {
-  if (nItems === Infinity) { throw new Error('cannot populate array infinitely') }
-  const result: any[] = []
-  for (let index = 0; index < nItems; index++) {
-    result.push(array[index % array.length])
-  }
-  return result
-}
