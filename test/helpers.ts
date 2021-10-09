9c6c88266e2a5f0e1e8a4882b84fdbc54f310d28
Misha Bruml <mishabruml@gmail.com>
2021-10-09 16:06:41 +0100

    delete helpers

--- test/helpers.ts
+++
@@ -1,9 +0,0 @@
-type RecursivePartial<T> = {
-  [K in keyof T]?: T[K] extends Array<infer U>
-    ? Array<RecursivePartial<U>>
-    : T[K] extends object
-      ? RecursivePartial<T[K]>
-      : T[K]
-}
-
-export const partial = <T>(partial?: RecursivePartial<T>): T => partial as any
